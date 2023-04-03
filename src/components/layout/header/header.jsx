54266835f10f722e59507ae2f2eae5d854751c90
sadegh <ghasemnejad.sadegh@gmail.com>
2023-04-03 11:18:38 +0330

    add header and add to apppjs

---
+++ src/components/layout/header/header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <ul className="header__list">
+        <li className="header__list__item">
+          <button className="header__list__item__btn--login">Login</button>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Header from "./components/layout/header/header";
 import "./sass/main.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <main>
+      <App />
+    </main>
   </React.StrictMode>
 );
